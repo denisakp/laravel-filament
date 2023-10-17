0842636cb0e3c7e53c814bd51fa06772f1ff728c
Denis AKPAGNONITE <akpagnonited@gmail.com>
2023-10-17 19:02:38 +0100

    fix warning messages

--- .github/scripts/git-update.sh
+++ .github/scripts/git-update.sh
@@ -3,16 +3,18 @@
 VERSION=""
 
 # get parameters
-while getopts v: flag
+while getopts v flag
 do
-  case "${flag}" in
+  case "$flag" in
     v) VERSION=${OPTARG};;
+    *) echo "usage: $0 [-v]" >&2
+        exit 1;;
   esac
 done
 
 # get highest tag number, and add v0.1.0 if doesn't exist
 git fetch --prune --unshallow 2>/dev/null
-CURRENT_VERSION=`git describe --abbrev=0 --tags 2>/dev/null`
+CURRENT_VERSION=$("git describe --abbrev=0 --tags 2>/dev/null")
 
 if [[ $CURRENT_VERSION == '' ]]
 then
@@ -21,34 +23,34 @@ fi
 echo "Current Version: $CURRENT_VERSION"
 
 # replace . with space so can split into an array
-CURRENT_VERSION_PARTS=(${CURRENT_VERSION//./ })
+CURRENT_VERSION_PARTS=("${CURRENT_VERSION//./ }")
 
 # get number parts
-VNUM1=${CURRENT_VERSION_PARTS[0]}
-VNUM2=${CURRENT_VERSION_PARTS[1]}
-VNUM3=${CURRENT_VERSION_PARTS[2]}
+MAJOR=${CURRENT_VERSION_PARTS[0]}
+MINOR=${CURRENT_VERSION_PARTS[1]}
+PATCH=${CURRENT_VERSION_PARTS[2]}
 
 if [[ $VERSION == 'major' ]]
 then
-  VNUM1=v$((VNUM1+1))
+  MAJOR=v$((MAJOR+1))
 elif [[ $VERSION == 'minor' ]]
 then
-  VNUM2=$((VNUM2+1))
+  MINOR=$((MINOR+1))
 elif [[ $VERSION == 'patch' ]]
 then
-  VNUM3=$((VNUM3+1))
+  PATCH=$((PATCH+1))
 else
   echo "No version type (https://semver.org/) or incorrect type specified, try: -v [major, minor, patch]"
   exit 1
 fi
 
 # create new tag
-NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
+NEW_TAG="$MAJOR.$MINOR.$PATCH"
 echo "($VERSION) updating $CURRENT_VERSION to $NEW_TAG"
 
 # get current hash and see if it already has a tag
-GIT_COMMIT=`git rev-parse HEAD`
-NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
+GIT_COMMIT=$("git rev-parse HEAD")
+NEEDS_TAG=$("git describe --contains $GIT_COMMIT 2>/dev/null")
 
 # only tag if no tag already
 if [ -z "$NEEDS_TAG" ]; then
@@ -60,6 +62,7 @@ else
   echo "Already a tag on this commit"
 fi
 
-echo ::set-output name=git-tag::$NEW_TAG
+#echo ::set-output name=git-tag::$NEW_TAG
+echo "name=$(git-tag::$NEW_TAG)" >>"$GITHUB_OUTPUT"
 
 exit 0
--- .github/scripts/tsk-semver.sh
+++ .github/scripts/tsk-semver.sh
@@ -4,19 +4,23 @@ TASK_DEFINITION_FILE=".github/aws/task-definition.json"
 SEMVER=""
 
 # get parameters
-while getopts v: flag
+while getopts v flag
 do
-  case "${flag}" in
+  case "$flag" in
     v) SEMVER=${OPTARG};;
+    *) echo "usage: $0 [-v]" >&2
+        exit 1;;
   esac
 done
 
-# Make sure that the task defintion file exists 
+# Make sure that the task definition file exists
 if [ -f "$TASK_DEFINITION_FILE" ]; then
     jq --arg newVersion "$SEMVER" '.containerDefinitions[0].image |= (sub(":latest$"; ":" + $newVersion))' "$TASK_DEFINITION_FILE" > tmp.json
 
-    # check if jq command is successfull then update the file
-    if [ $? -eq 0 ]; then
+    jq_exit_code=$?
+
+    # check if jq command is successfully then update the file
+    if [ "$jq_exit_code" -eq 0 ]; then
         mv tmp.json "$TASK_DEFINITION_FILE"
         echo "Set TAG to $SEMVER"
     else
